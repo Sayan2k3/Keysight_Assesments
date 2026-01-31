abf4cc60f385feec5026b530e88ab5eeda248809
Sayan Paul <sayan.paul@keysight.com>
2026-01-31 16:36:02 +0530

    assigment upploaded

---
+++ springboot_assesment/first_project1-2/src/main/java/com/example/demo/FirstProject12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProject12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProject12Application.class, args);
+	}
+
+}
